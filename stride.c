f2ad5a3c9119dbbfa9ca67852a6dc7b13e612bfc
Mark A. Sheldon <msheldon@cs.tufts.edu>
2013-10-04 02:22:50 -0400

    update from mkfile Fri Oct  4 02:22:50 EDT 2013

--- stride.c
+++ stride.c
@@ -1,55 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <assert.h>
 #include <time.h>
 
-static char xor(const char *p, const char *limit, int stride)
-{
-        assert(stride > 0);
-        char sum = 0;
+static void run_stride_tests(size_t bytes, double megabytes,
+                             int stride,   char *mem);
+static char xor(const char *p, const char *limit, int stride);
 
-        for (int offset = 0; offset < stride; offset++)
-                for (const char *s = p + offset; s < limit; s += stride)
-                        sum ^= *s;
-        return sum;
-}
 
-volatile int sink;        /* keeps result from being optimized away */
+static volatile int sink;        /* keeps result from being optimized away */
 
 int main(int argc, char *argv[]) 
 {
+        double megabytes;
+        int    stride;
+        size_t bytes;
+        char  *p_mem;
+
         if (argc != 3) {
                 fprintf(stderr, "Usage: %s megabytes stride\n", argv[0]);
-                exit(1);
+                exit(EINVAL);
         }
-        double megabytes = atof(argv[1]);
-        int    stride    = atoi(argv[2]);               /* never use atoi() */
+        megabytes = atof(argv[1]);               /* never use atoi() */
+        stride    = atoi(argv[2]);               /* never use atoi() */
+
         assert(megabytes > 0 && stride > 0);
 
-        size_t bytes = megabytes * 1024 * 1024;
-        char  *p     = malloc(bytes);
+        bytes = megabytes * 1024 * 1024;
+        p_mem = malloc(bytes);
 
-        if (p == NULL) {
+        if (p_mem == NULL) {
                 if ((double)(size_t) megabytes == megabytes)
                         fprintf(stderr, "%s: Cannot allocate %.2fMiB\n",
                                 argv[0], megabytes);
                 else
                         fprintf(stderr, "%s: Cannot allocate %dMiB\n",
                                 argv[0], (int)megabytes);
-                exit(2);
+                exit(ENOMEM);
         }
+        run_stride_tests(bytes, megabytes, stride, p_mem);
 
-        clock_t start = clock();
-        int     iter  = 500 / megabytes;
+        return 0;
+}
 
-        if (iter < 5)
-                iter = 5;
+static void run_stride_tests(size_t bytes, double megabytes,
+                             int stride, char *mem)
+{
+        clock_t start = clock(),
+                stop;
+        int     i, iter  = 500 / megabytes;
+        double  loads, seconds;
 
-        for (int i = 0; i < iter; i++) 
-                sink = xor(p, p + bytes, stride);
-        clock_t stop    = clock();
-        double  loads   = (double) bytes * iter;
-        double  seconds = (double)(stop - start) / (double)CLOCKS_PER_SEC;
+        if (iter < 5) iter = 5;
+
+        for (i = 0; i < iter; i++) 
+                sink = xor(mem, mem + bytes, stride);
+
+        stop    = clock();
+        loads   = (double) bytes * iter;
+        seconds = (double)(stop - start) / (double)CLOCKS_PER_SEC;
 
         if ((double)(size_t) megabytes == megabytes)
                 printf("%dMiB", (int) megabytes);
@@ -59,7 +69,18 @@ int main(int argc, char *argv[])
         printf(" stride %d results in %5.2fns CPU time per load"
                " (total %.3fs)\n",
                stride, seconds/1e-9/loads, seconds);
-
-        return 0;
 }
   
+static char xor(const char *p, const char *limit, int stride)
+{
+        char        sum = 0;
+        int         offset;
+        const char *s;
+
+        assert(stride > 0);
+
+        for (offset = 0; offset < stride; offset++)
+                for (s = p + offset; s < limit; s += stride)
+                        sum ^= *s;
+        return sum;
+}
