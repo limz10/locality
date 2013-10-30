9f07c77acbbbf46213e801e6e7944441bac89495
Mark A. Sheldon <msheldon@cs.tufts.edu>
2013-10-30 00:18:30 -0400

    reformatted code

--- ppmtrans.c
+++ ppmtrans.c
@@ -8,12 +8,13 @@
 #include "a2blocked.h"
 #include "pnm.h"
 
-int main(int argc, char *argv[]) {
-  int rotation = 0;
-  A2Methods_T methods = uarray2_methods_plain; // default to UArray2 methods
-  assert(methods);
-  A2Methods_mapfun *map = methods->map_default; // default to best map
-  assert(map);
+int main(int argc, char *argv[])
+{
+	int rotation = 0;
+	A2Methods_T methods = uarray2_methods_plain;	// default to UArray2 methods
+	assert(methods);
+	A2Methods_mapfun *map = methods->map_default;	// default to best map
+	assert(map);
 #define SET_METHODS(METHODS, MAP, WHAT) do { \
       methods = (METHODS); \
       assert(methods); \
@@ -24,31 +25,36 @@ int main(int argc, char *argv[]) {
       } \
     } while(0)
 
-  int i;
-  for (i = 1; i < argc; i++) {
-    if (!strcmp(argv[i], "-row-major")) {
-      SET_METHODS(uarray2_methods_plain, map_row_major, "row-major");
-    } else if (!strcmp(argv[i], "-col-major")) {
-      SET_METHODS(uarray2_methods_plain, map_col_major, "column-major");
-    } else if (!strcmp(argv[i], "-block-major")) {
-      SET_METHODS(uarray2_methods_blocked, map_block_major, "block-major");
-    } else if (!strcmp(argv[i], "-rotate")) {
-      assert(i + 1 < argc);
-      char *endptr;
-      rotation = strtol(argv[++i], &endptr, 10);
-      assert(*endptr == '\0'); // parsed all correctly
-      assert(rotation == 0   || rotation == 90
-          || rotation == 180 || rotation == 270);
-    } else if (*argv[i] == '-') {
-      fprintf(stderr, "%s: unknown option '%s'\n", argv[0], argv[i]);
-      exit(1);
-    } else if (argc - i > 2) {
-      fprintf(stderr, "Usage: %s [-rotate <angle>] "
-              "[-{row,col,block}-major] [filename]\n", argv[0]);
-      exit(1);
-    } else {
-      break;
-    }
-  }
-   assert(0); // the rest of this function is not yet implemented
+	int i;
+	for (i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "-row-major")) {
+			SET_METHODS(uarray2_methods_plain, map_row_major,
+				    "row-major");
+		} else if (!strcmp(argv[i], "-col-major")) {
+			SET_METHODS(uarray2_methods_plain, map_col_major,
+				    "column-major");
+		} else if (!strcmp(argv[i], "-block-major")) {
+			SET_METHODS(uarray2_methods_blocked, map_block_major,
+				    "block-major");
+		} else if (!strcmp(argv[i], "-rotate")) {
+			assert(i + 1 < argc);
+			char *endptr;
+			rotation = strtol(argv[++i], &endptr, 10);
+			assert(*endptr == '\0');	// parsed all correctly
+			assert(rotation == 0 || rotation == 90
+			       || rotation == 180 || rotation == 270);
+		} else if (*argv[i] == '-') {
+			fprintf(stderr, "%s: unknown option '%s'\n", argv[0],
+				argv[i]);
+			exit(1);
+		} else if (argc - i > 2) {
+			fprintf(stderr, "Usage: %s [-rotate <angle>] "
+				"[-{row,col,block}-major] [filename]\n",
+				argv[0]);
+			exit(1);
+		} else {
+			break;
+		}
+	}
+	assert(0);		// the rest of this function is not yet implemented
 }
