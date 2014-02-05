5070268c7dd9e6eaf7c249e4a474325817dbb2d6
Noah Mendelsohn <noah@cs.tufts.edu>
2014-02-05 12:47:50 -0500

    fixed argument handlijng

--- ppmtrans.c
+++ ppmtrans.c
@@ -8,24 +8,21 @@
 #include "a2blocked.h"
 #include "pnm.h"
 
+static void
+usage(const char *progname)
+{
+	fprintf(stderr, "Usage: %s [-rotate <angle>] "
+		"[-{row,col,block}-major] [filename]\n",
+		progname);
+	exit(1);
+}
+
 int main(int argc, char *argv[])
 {
 	int rotation = 0;
-	A2Methods_T methods = uarray2_methods_plain;	// default to UArray2 methods
-	assert(methods);
-	A2Methods_mapfun *map = methods->map_default;	// default to best map
-	assert(map);
-#define SET_METHODS(METHODS, MAP, WHAT) do { \
-      methods = (METHODS); \
-      assert(methods); \
-      map = methods->MAP; \
-      if (!map) { \
-        fprintf(stderr, "%s does not support " WHAT "mapping\n", argv[0]); \
-        exit(1); \
-      } \
-    } while(0)
-
 	int i;
+
+
 	for (i = 1; i < argc; i++) {
 		if (!strcmp(argv[i], "-row-major")) {
 			SET_METHODS(uarray2_methods_plain, map_row_major,
@@ -37,24 +34,30 @@ int main(int argc, char *argv[])
 			SET_METHODS(uarray2_methods_blocked, map_block_major,
 				    "block-major");
 		} else if (!strcmp(argv[i], "-rotate")) {
-			assert(i + 1 < argc);
+			if (!(i + 1 < argc)) {      /* no rotate value */
+				usage(argv[0]);
+			}
 			char *endptr;
 			rotation = strtol(argv[++i], &endptr, 10);
-			assert(*endptr == '\0');	// parsed all correctly
-			assert(rotation == 0 || rotation == 90
-			       || rotation == 180 || rotation == 270);
+			if (!(rotation == 0 || rotation == 90
+			      || rotation == 180 || rotation == 270)) {
+				fprintf(stderr, "Rotation must be "
+					"0, 90 180 or 270\n");
+				usage(argv[0]);
+			}
+			if (!(*endptr == '\0')) {    /* Not a number */
+				usage(argv[0]);
+			}
 		} else if (*argv[i] == '-') {
 			fprintf(stderr, "%s: unknown option '%s'\n", argv[0],
 				argv[i]);
-			exit(1);
-		} else if (argc - i > 2) {
-			fprintf(stderr, "Usage: %s [-rotate <angle>] "
-				"[-{row,col,block}-major] [filename]\n",
-				argv[0]);
-			exit(1);
+		} else if (argc - i > 1) {
+			fprintf(stderr, "Too many arguments\n");
+			usage(argv[0]);
 		} else {
 			break;
 		}
 	}
+	printf("It worked\n");
 	assert(0);		// the rest of this function is not yet implemented
 }
